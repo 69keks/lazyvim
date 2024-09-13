c9cf9e4abaa6697035019f7e5946b69a458aaf4a
Leviathan melvilli <mehere.youthere@pm.me>
2024-09-13 21:49:00 +0530

    Don't need neotree transparency settings anymore

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,15 +4,3 @@
 
 -- Disable stupid current line highlighting as it looks ugly against transparent and blurred backgrounds
 vim.opt.cursorline = false
-
--- Setting NeoTree background to transparent
-vim.api.nvim_create_augroup("nobg", { clear = true })
-vim.api.nvim_create_autocmd("ColorScheme", {
-  desc = "Make NeoTree background transparent",
-  group = "nobg",
-  pattern = "*",
-  callback = function()
-    vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = "NONE", ctermbg = "NONE" })
-    vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "NONE", ctermbg = "NONE" })
-  end,
-})
