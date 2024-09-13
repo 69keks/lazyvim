c29dba6870d99eb29567db3a4e5cc5e6ff850f68
Leviathan melvilli <mehere.youthere@pm.me>
2024-09-13 21:46:23 +0530

    Attach colorizer to buffer automagically

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -1,5 +1,10 @@
 return {
   {
     "norcalli/nvim-colorizer.lua",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      vim.o.termguicolors = true
+      require("colorizer").setup()
+    end,
   },
 }
