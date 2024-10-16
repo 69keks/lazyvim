c0f8e7bde9082ca01938390ae87b5ace7ea52b16
Leviathan melvilli <mehere.youthere@pm.me>
2024-10-16 07:58:35 +0530

    Slight changes to tokyonight
    Changed bg to transparent instead of #0f111b.
    Enabled italics globally.

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -3,12 +3,12 @@ return {
     "folke/tokyonight.nvim",
     opts = {
       transparent = true,
-      on_colors = function(colors)
-        colors.bg = "#0f111b"
-      end,
+      -- on_colors = function(colors)
+      --   colors.bg = "#0f111b"
+      -- end,
       styles = {
         comments = {
-          italic = false,
+          italic = true,
         },
         keywords = {
           italic = false,
