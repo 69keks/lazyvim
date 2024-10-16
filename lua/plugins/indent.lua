f8da7a8296d630161beb4daf45191c8327c3d5a1
Leviathan melvilli <mehere.youthere@pm.me>
2024-10-16 07:55:25 +0530

    Switch to tokyonight
    Tokyonight colors for lazyvim, indent-blankline and lualine.
    BG is #0f111b or transparent when needed.
    Variant is tokyonight-night.

--- lua/plugins/indent.lua
+++ lua/plugins/indent.lua
@@ -14,13 +14,13 @@ return {
       }
       local hooks = require("ibl.hooks")
       hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-        vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#ea6962" })
-        vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#fabd2f" })
-        vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#458588" })
-        vim.api.nvim_set_hl(0, "RainbowTeal", { fg = "#8ec07c" })
-        vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#b8bb26" })
-        vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#e78a4e" })
-        vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#83a598" })
+        vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#f7768e" })
+        vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#e0af68" })
+        vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#7aa2f7" })
+        vim.api.nvim_set_hl(0, "RainbowTeal", { fg = "#1abc9c" })
+        vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#9ece6a" })
+        vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#ff9e64" })
+        vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#7dcfff" })
       end)
 
       local ibl = require("ibl")
@@ -39,7 +39,7 @@ return {
   },
   {
     "echasnovski/mini.indentscope",
-    version = false, -- wait till new 0.7.0 release to put it back on semver
+    version = false,
     event = { "BufReadPre", "BufNewFile" },
     opts = {
       symbol = "│",
@@ -49,7 +49,7 @@ return {
       vim.api.nvim_create_autocmd("FileType", {
         pattern = {
           "help",
-          "dashboard",
+          -- "dashboard",
           "neo-tree",
           "Trouble",
           "lazy",
